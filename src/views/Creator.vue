421ae1a614efae2a7f7d28827bf417781de73805
mfvodka107 <giang.dlinh92@gmail.com>
2020-09-11 14:27:52 +0700

    replace tiptap with tui-editor

--- src/views/Creator.vue
+++ src/views/Creator.vue
@@ -5,7 +5,11 @@
       <div
         class="w-full container mx-auto flex flex-wrap items-center mt-0 px-4 justify-between sm:justify-center"
       >
-        <label for="menu-toggle" class="cursor-pointer py-2" @click="$router.go(-1)">
+        <label
+          for="menu-toggle"
+          class="cursor-pointer py-2"
+          @click="$router.go(-1)"
+        >
           <svg
             class="color-black fill-current w-5"
             xmlns="http://www.w3.org/2000/svg"
@@ -40,123 +44,7 @@
       <div class="mx-auto max-w-lg">
         <div class="py-1">
           <span class="px-1 text-sm text-gray-600">Nội dung</span>
-          <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
-            <div class="menubar">
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.bold() }"
-                @click="commands.bold"
-              >
-                <icon name="bold" />
-              </button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.italic() }"
-                @click="commands.italic"
-              >
-                <icon name="italic" />
-              </button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.strike() }"
-                @click="commands.strike"
-              >
-                <icon name="strike" />
-              </button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.underline() }"
-                @click="commands.underline"
-              >
-                <icon name="underline" />
-              </button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.code() }"
-                @click="commands.code"
-              >
-                <icon name="code" />
-              </button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.paragraph() }"
-                @click="commands.paragraph"
-              >
-                <icon name="paragraph" />
-              </button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.heading({ level: 1 }) }"
-                @click="commands.heading({ level: 1 })"
-              >H1</button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.heading({ level: 2 }) }"
-                @click="commands.heading({ level: 2 })"
-              >H2</button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.heading({ level: 3 }) }"
-                @click="commands.heading({ level: 3 })"
-              >H3</button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.bullet_list() }"
-                @click="commands.bullet_list"
-              >
-                <icon name="ul" />
-              </button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.ordered_list() }"
-                @click="commands.ordered_list"
-              >
-                <icon name="ol" />
-              </button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.blockquote() }"
-                @click="commands.blockquote"
-              >
-                <icon name="quote" />
-              </button>
-
-              <button
-                class="menubar__button"
-                :class="{ 'is-active': isActive.code_block() }"
-                @click="commands.code_block"
-              >
-                <icon name="code" />
-              </button>
-
-              <button class="menubar__button" @click="commands.horizontal_rule">
-                <icon name="hr" />
-              </button>
-
-              <button class="menubar__button" @click="commands.undo">
-                <icon name="undo" />
-              </button>
-
-              <button class="menubar__button" @click="commands.redo">
-                <icon name="redo" />
-              </button>
-            </div>
-          </editor-menu-bar>
-          <editor-content
-            :editor="editor"
-            class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-          />
+          <div id="editor"></div>
         </div>
         <div class="py-1">
           <span class="px-1 text-sm text-gray-600">Tag</span>
@@ -240,6 +128,7 @@
 
         <button
           class="mt-3 text-lg font-semibold w-full text-white rounded-lg px-6 py-3 btn-hover gradient-black"
+          @click="submitContent"
         >
           <svg
             class="text-white w-5 inline"
@@ -264,63 +153,34 @@
 
 <script>
 import logo from "@/components/Logo.vue";
-import Icon from "@/components/Creator/Icon.vue";
-import { Editor, EditorContent, EditorMenuBar } from "tiptap";
-import {
-  Blockquote,
-  CodeBlock,
-  HardBreak,
-  Heading,
-  HorizontalRule,
-  OrderedList,
-  BulletList,
-  ListItem,
-  TodoItem,
-  TodoList,
-  Bold,
-  Code,
-  Italic,
-  Link,
-  Strike,
-  Underline,
-  History,
-} from "tiptap-extensions";
+import Editor from "@toast-ui/editor";
+import "codemirror/lib/codemirror.css"; // Editor's Dependency Style
+import "@toast-ui/editor/dist/toastui-editor.css"; // Editor's Style
+
 export default {
   components: {
-    logo,
-    Icon,
-    EditorMenuBar,
-    EditorContent,
+    logo
   },
   data() {
     return {
-      editor: new Editor({
-        extensions: [
-          new Blockquote(),
-          new BulletList(),
-          new CodeBlock(),
-          new HardBreak(),
-          new Heading({ levels: [1, 2, 3] }),
-          new HorizontalRule(),
-          new ListItem(),
-          new OrderedList(),
-          new TodoItem(),
-          new TodoList(),
-          new Link(),
-          new Bold(),
-          new Code(),
-          new Italic(),
-          new Strike(),
-          new Underline(),
-          new History(),
-        ],
-        content: "<p>This is just a boring paragraph</p>",
-      }),
+      editor: null
     };
   },
-  beforeDestroy() {
-    // Always destroy your editor instance when it's no longer needed
-    this.editor.destroy();
+  mounted() {
+    this.editor = new Editor({
+      el: document.querySelector("#editor"),
+      height: "300px",
+      initialEditType: "markdown",
+      previewStyle: "wysiwyg"
+    });
+    // this.editor.getHtml();
   },
+  methods: {
+    submitContent() {
+      let content = this.editor.getHtml();
+      console.log(content);
+    }
+  },
+  beforeDestroy() {}
 };
 </script>
