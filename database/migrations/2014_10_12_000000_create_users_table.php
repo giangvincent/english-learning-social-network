b22de8de5a5b3b2edc32a1b9fa9a941f012154ad
giangvincent <giang.dlinh92@gmail.com>
2020-09-12 09:23:37 +0700

    init admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('nick_name')->nullable();
+            $table->string('full_name');
+            $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->string('avatar')->nullable();
+            $table->string('birthday')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('facebook')->nullable();
+            
             $table->rememberToken();
             $table->timestamps();
         });
