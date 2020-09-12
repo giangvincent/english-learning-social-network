1ec0408589d5ead579cef960f294af7f3918fd05
giangvincent <giang.dlinh92@gmail.com>
2020-09-12 09:38:01 +0700

    change admin config

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,7 +42,12 @@ return [
     */
 
     'disks' => [
-
+        'admin' => [
+            'driver' => 'local',
+            'root' => public_path('admin'),
+            'url' => env('APP_URL').'/admin',
+            'visibility' => 'public',
+        ],
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
@@ -50,8 +55,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path('upload'),
+            'url' => env('APP_URL').'/upload',
             'visibility' => 'public',
         ],
 
