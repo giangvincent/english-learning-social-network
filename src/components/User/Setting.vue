3298b1b5df37345027db86509d7e3880e5837b00
mfvodka107 <giang.dlinh92@gmail.com>
2020-10-30 15:39:25 +0700

    fix setting page

--- src/components/User/Setting.vue
+++ src/components/User/Setting.vue
@@ -1,111 +1,118 @@
 <template>
-  <div>
-    <div class="bg-white md:w-6/12 mx-auto mb-20 px-8 py-6 rounded shadow">
-      <fieldset class="relative my-2 p-3 border-2 border-blue-900 rounded-lg">
-        <legend class="mx-2 font-bold rounded-lg bg-blue-900 text-white p-2">
-          Đổi thông tin cơ bản
-        </legend>
-        <div class="py-1">
-          <span class="px-1 text-sm text-gray-600">Tên đăng nhập</span>
-          <input
-            placeholder
-            type="text"
-            class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-          />
-        </div>
-        <div class="py-1">
-          <span class="px-1 text-sm text-gray-600">Tên đầy đủ</span>
-          <input
-            placeholder
-            type="text"
-            class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-          />
-        </div>
-        <div class="py-1">
-          <span class="px-1 text-sm text-gray-600">Tên hiển thị</span>
-          <input
-            placeholder
-            type="text"
-            class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-          />
-        </div>
-        <div class="py-1">
-          <span class="px-1 text-sm text-gray-600">Email</span>
-          <input
-            placeholder
-            readonly
-            type="text"
-            class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-          />
-        </div>
-        <div class="py-1">
-          <span class="px-1 text-sm text-gray-600">Tài khoản Facebook</span>
-          <input
-            placeholder
-            type="text"
-            class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-          />
-        </div>
-        <div class="py-1">
-          <span class="px-1 text-sm text-gray-600">Giới thiệu ngắn</span>
-          <textarea
-            placeholder
-            class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-          ></textarea>
-        </div>
-        <div class="py-1">
-          <span class="px-1 text-sm text-gray-600">Loại tài khoản</span>
-          <select
-            class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-          >
-            <option>Cá nhân</option>
-            <option>Tổ chức</option>
-          </select>
-        </div>
-      </fieldset>
-      <fieldset class="relative my-2 p-3 border-2 border-green-800 rounded-lg">
-        <legend class="mx-2 font-bold rounded-lg bg-green-800 text-white p-2">
-          Đổi cài đặt thông báo
-        </legend>
-      </fieldset>
-      <fieldset class="relative my-2 p-3 border-2 border-red-600 rounded-lg">
-        <legend class="mx-2 font-bold rounded-lg bg-red-600 text-white p-2">
-          Đổi mật khẩu
-        </legend>
-        <div class="py-1">
-          <span class="px-1 text-sm text-gray-600">Mật khẩu cũ</span>
-          <input
-            placeholder
-            type="password"
-            x-model="password"
-            class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-          />
-        </div>
-        <div class="py-1">
-          <span class="px-1 text-sm text-gray-600">Mật khẩu mới</span>
-          <input
-            placeholder
-            type="password"
-            x-model="password"
-            class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-          />
-        </div>
-        <div class="py-1">
-          <span class="px-1 text-sm text-gray-600">Xác nhận mật khẩu mới</span>
-          <input
-            placeholder
-            type="password"
-            x-model="password_confirm"
-            class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
-          />
-        </div>
-      </fieldset>
+  <div class="flex">
+    <div class="hidden md:block md:w-1/5"></div>
+    <div class=" w-full px-4 my-10 md:my-6 md:px-6 lg:px-8 md:w-4/5 ">
+      <div class="p-4 md:p-6 rounded shadow bg-white">
+        <fieldset class="relative my-2 p-3 border-2 border-blue-900 rounded-lg">
+          <legend class="mx-2 font-bold rounded-lg bg-blue-900 text-white p-2">
+            Đổi thông tin cơ bản
+          </legend>
+          <div class="py-1">
+            <span class="px-1 text-sm text-gray-600">Tên đăng nhập</span>
+            <input
+              placeholder
+              type="text"
+              class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+            />
+          </div>
+          <div class="py-1">
+            <span class="px-1 text-sm text-gray-600">Tên đầy đủ</span>
+            <input
+              placeholder
+              type="text"
+              class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+            />
+          </div>
+          <div class="py-1">
+            <span class="px-1 text-sm text-gray-600">Tên hiển thị</span>
+            <input
+              placeholder
+              type="text"
+              class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+            />
+          </div>
+          <div class="py-1">
+            <span class="px-1 text-sm text-gray-600">Email</span>
+            <input
+              placeholder
+              readonly
+              type="text"
+              class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+            />
+          </div>
+          <div class="py-1">
+            <span class="px-1 text-sm text-gray-600">Tài khoản Facebook</span>
+            <input
+              placeholder
+              type="text"
+              class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+            />
+          </div>
+          <div class="py-1">
+            <span class="px-1 text-sm text-gray-600">Giới thiệu ngắn</span>
+            <textarea
+              placeholder
+              class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+            ></textarea>
+          </div>
+          <div class="py-1">
+            <span class="px-1 text-sm text-gray-600">Loại tài khoản</span>
+            <select
+              class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+            >
+              <option>Cá nhân</option>
+              <option>Tổ chức</option>
+            </select>
+          </div>
+        </fieldset>
+        <fieldset
+          class="relative my-2 p-3 border-2 border-green-800 rounded-lg"
+        >
+          <legend class="mx-2 font-bold rounded-lg bg-green-800 text-white p-2">
+            Đổi cài đặt thông báo
+          </legend>
+        </fieldset>
+        <fieldset class="relative my-2 p-3 border-2 border-red-600 rounded-lg">
+          <legend class="mx-2 font-bold rounded-lg bg-red-600 text-white p-2">
+            Đổi mật khẩu
+          </legend>
+          <div class="py-1">
+            <span class="px-1 text-sm text-gray-600">Mật khẩu cũ</span>
+            <input
+              placeholder
+              type="password"
+              x-model="password"
+              class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+            />
+          </div>
+          <div class="py-1">
+            <span class="px-1 text-sm text-gray-600">Mật khẩu mới</span>
+            <input
+              placeholder
+              type="password"
+              x-model="password"
+              class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+            />
+          </div>
+          <div class="py-1">
+            <span class="px-1 text-sm text-gray-600"
+              >Xác nhận mật khẩu mới</span
+            >
+            <input
+              placeholder
+              type="password"
+              x-model="password_confirm"
+              class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none"
+            />
+          </div>
+        </fieldset>
 
-      <button
-        class="mt-3 text-lg font-semibold w-full text-white rounded-lg px-6 py-3 btn-hover gradient-black"
-      >
-        Lưu thay đổi
-      </button>
+        <button
+          class="mt-3 text-lg font-semibold w-full text-white rounded-lg px-6 py-3 btn-hover gradient-black"
+        >
+          Lưu thay đổi
+        </button>
+      </div>
     </div>
   </div>
 </template>
