f0160bfbde668a717b5709f3d492eebdac75b519
mfvodka107 <giang.dlinh92@gmail.com>
2020-11-10 20:03:10 +0700

    update multiple quizs

--- src/components/Feed/Quiz.vue
+++ src/components/Feed/Quiz.vue
@@ -6,44 +6,83 @@
       <h2>{{ postData.subject }}</h2>
     </div>
     <CatsAndTags :postData="postData"></CatsAndTags>
+    <div
+      ref="frontCard"
+      class="flex flex-col m-2 shadow rounded"
+      :class="{ block: !currentBackCard, hidden: currentBackCard }"
+    >
+      <section>
+        <div class="ql-snow" style="height: auto; border: none">
+          <div
+            class="ql-editor"
+            v-html="postData.content[currentQuizIndex].contentHtml"
+            style="height: auto"
+          ></div>
+        </div>
+      </section>
 
-    <div class="px-3 pb-4" v-html="postData.content[0].contentHtml"></div>
-    <!-- End content text -->
-    <div class="mx-auto bg-color-black mb-4">
-      <img
-        v-for="(image, imgIndex) in postData.content[0].images"
-        :key="`content.images.${imgIndex}`"
-        class="w-full"
-        :src="rootUrl + image"
-      />
-    </div>
-    <!-- End media -->
-
-    <div class="py-3 text-sm">
-      <span class="px-2">Các đáp án là :</span>
-      <div
-        class="flex justify-start cursor-pointer text-md rounded-lg border-2 hover:border-gray-600 py-2 m-2"
-        :class="{
-          'border-gray-300':
-            !postData.content[0].correctAnswers.includes(index) &&
-            (!answered || numAnswer !== index),
-          'border-black': answered && numAnswer === index,
-          'bg-white': !postData.content[0].correctAnswers.includes(index),
-          'border-green-800 bg-green-400':
-            reviewCorrectAns &&
-            postData.content[0].correctAnswers.includes(numAnswer) &&
-            numAnswer === index,
-          'border-red-700 bg-red-400':
-            reviewCorrectAns &&
-            !postData.content[0].correctAnswers.includes(numAnswer) &&
-            numAnswer !== index
-        }"
-        v-for="(answer, index) in postData.content[0].answers"
-        :key="'answers-' + index"
-        @click="pickAnswer(index)"
-      >
-        <div class="px-2">{{ answer }}</div>
+      <!-- End content text -->
+      <div class="mx-auto bg-color-black mb-4">
+        <img
+          v-for="(image, imgIndex) in postData.content[currentQuizIndex].images"
+          :key="`content.images.${imgIndex}`"
+          class="w-full"
+          :src="rootUrl + image"
+        />
       </div>
+      <!-- End media -->
+
+      <div class="py-3 text-sm">
+        <span class="px-2">Các đáp án là :</span>
+        <div
+          class="flex justify-start cursor-pointer text-md rounded-lg border-2 hover:border-gray-600 py-2 m-2"
+          :class="{
+            'border-gray-300':
+              !postData.content[currentQuizIndex].correctAnswers.includes(
+                index
+              ) &&
+              (!answered || numAnswer !== index),
+            'border-black': answered && numAnswer === index,
+            'bg-white': !postData.content[
+              currentQuizIndex
+            ].correctAnswers.includes(index),
+            'border-green-800 bg-green-400':
+              reviewCorrectAns &&
+              postData.content[currentQuizIndex].correctAnswers.includes(
+                numAnswer
+              ) &&
+              numAnswer === index,
+            'border-red-700 bg-red-400':
+              reviewCorrectAns &&
+              !postData.content[currentQuizIndex].correctAnswers.includes(
+                numAnswer
+              ) &&
+              numAnswer !== index
+          }"
+          v-for="(answer, index) in postData.content[currentQuizIndex].answers"
+          :key="'answers-' + index"
+          @click="pickAnswer(index)"
+        >
+          <div class="px-2">{{ answer }}</div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class=" mb-4 flex cursor-pointer justify-center items-center flex-wrap"
+    >
+      <div
+        class="bg-gray-700 border-2 border-white text-white text-center font-bold rounded-full py-1 px-3"
+      >
+        Quiz {{ currentQuizIndex + 1 }}/{{ postData.content.length }}
+      </div>
+      <button
+        class="md:text-lg font-semibold text-white rounded-lg btn-hover gradient-black w-1/4 flex justify-center items-center py-1"
+        @click="toNextQuiz()"
+        v-show="reviewCorrectAns"
+      >
+        Next
+      </button>
     </div>
 
     <author
@@ -77,6 +116,7 @@ export default {
   },
   data() {
     return {
+      currentQuizIndex: 0,
       reviewCorrectAns: false,
       answered: false,
       numAnswer: null,
@@ -93,7 +133,7 @@ export default {
         content: [
           {
             contentHtml: "",
-
+            contentOrigin: { ops: [] },
             images: [],
             answers: [],
             correctAnswers: []
@@ -152,6 +192,15 @@ export default {
           self.reviewCorrectAns = true;
         }, 1000);
       }
+    },
+    toNextQuiz() {
+      this.reviewCorrectAns = false;
+      this.answered = false;
+      this.numAnswer = null;
+      this.currentQuizIndex =
+        this.currentQuizIndex < this.postData.content.length - 1
+          ? this.currentQuizIndex + 1
+          : this.currentQuizIndex;
     }
   }
 };
