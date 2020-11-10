8d616848824f6b97af4578921ed387b4ec003196
mfvodka107 <giang.dlinh92@gmail.com>
2020-11-10 16:12:24 +0700

    add quilljs

--- config/admin.php
+++ config/admin.php
@@ -401,5 +401,42 @@ return [
             // 如果要关掉这个扩展，设置为false
             'enable' => true,
         ],
+        'quill' => [
+            // If the value is set to false, this extension will be disabled
+            'enable' => true,
+            'config' => [
+                'modules' => [
+                    'syntax' => true,
+                    'toolbar' =>
+                    [
+                        ['size' => []],
+                        ['header' => []],
+                        'bold',
+                        'italic',
+                        'underline',
+                        'strike',
+                        ['script' => 'super'],
+                        ['script' => 'sub'],
+                        ['color' => []],
+                        ['background' => []],
+                        'blockquote',
+                        'code-block',
+                        ['list' => 'ordered'],
+                        ['list' => 'bullet'],
+                        ['indent' => '-1'],
+                        ['indent' => '+1'],
+                        'direction',
+                        ['align' => []],
+                        'link',
+                        'image',
+                        'video',
+                        'formula',
+                        'clean',
+                    ],
+                ],
+                'theme' => 'snow',
+                'height' => '200px',
+            ],
+        ],
     ],
 ];
