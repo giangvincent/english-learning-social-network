c33d33a829e78f48bc0637f5ca984e3c923b8e6b
mfvodka107 <giang.dlinh92@gmail.com>
2020-11-25 14:34:48 +0700

    fix left side panel

--- src/components/FooterInfo.vue
+++ src/components/FooterInfo.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="bottom-0 p-2 text-xs z-10 bg-white fixed w-64 md:w-1/5 lg:w-1/6">
-    <ul
-      class="flex flex-wrap my-1 items-center justify-center underline space-x-2"
-    >
-      <li>About</li>
-      <li>FAQs</li>
-      <li>Liên hệ</li>
+    <ul class="flex flex-wrap my-1 items-center underline">
+      <li class="mr-2">About</li>
+      <li class="mr-2">FAQs</li>
+      <li class="mr-2">Liên hệ</li>
       <li>Điều khoản & Chính sách</li>
     </ul>
-    <div class="text-center italic">
-      Copy right @Thatsgood 2020.
+    <div class="italic">
+      Copy right @Thatsgood {{ new Date().getFullYear() }}.
     </div>
   </div>
 </template>
