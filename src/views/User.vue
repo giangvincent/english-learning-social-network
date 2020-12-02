08ddb653322d02fc748db2d084b46117e64133a7
mfvodka107 <giang.dlinh92@gmail.com>
2020-12-02 16:28:36 +0700

    add cropper to user page

--- src/views/User.vue
+++ src/views/User.vue
@@ -13,6 +13,7 @@
           >
             <label
               class="m-1 absolute bg-gray-600 font-semibold h-6 p-1 right-0 rounded-full text-center text-white text-xs top-0 w-6 hover:bg-gray-300"
+              @click="selectImage('cover')"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -42,6 +43,7 @@
               />
               <label
                 class="absolute bg-gray-600 font-semibold h-6 p-1 right-0 rounded-full text-center text-white text-xs top-0 w-6 hover:bg-gray-300"
+                @click="selectImage('avatar')"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -122,6 +124,47 @@
     <saved-post v-if="$route.query.cur === 'saved'"></saved-post>
     <setting v-if="$route.query.cur === 'setting'"></setting>
     <to-creator></to-creator>
+    <input
+      ref="imageInput"
+      type="file"
+      class="hidden"
+      accept="image/*"
+      @change="onImageSelect"
+    />
+    <div
+      class="modal-bg transition-all duration-500 fixed left-0 overflow-auto z-50 top-0 bottom-0 right-0 flex"
+      v-show="showModal"
+    >
+      <div
+        class="absolute w-full h-full bg-opacity-50 bg-black"
+        @click="showModal = false"
+      ></div>
+      <div
+        class="modal-content bg-white relative m-auto w-4/5 max-w-lg shadow-lg rounded-lg"
+      >
+        <div class="py-3 px-3 flex flex-wrap">
+          <vue-cropper
+            ref="cropper"
+            :aspect-ratio="aspectRatio"
+            :src="cropperImg"
+          />
+        </div>
+        <div class="mb-3 flex flex-wrap justify-center  text-white">
+          <button
+            class="font-semibold rounded-lg btn-hover gradient-black px-3 py-1"
+            @click="cropImage"
+          >
+            Cắt và tải hình lên
+          </button>
+          <button
+            class="font-semibold rounded-lg btn-hover bg-gray-900 px-3 py-1"
+            @click="showModal = false"
+          >
+            Hủy
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -132,16 +175,24 @@ import SavedPost from "@/components/User/SavedPost.vue";
 import UserCreated from "@/components/User/UserCreated.vue";
 import Setting from "@/components/User/Setting.vue";
 
+import VueCropper from "vue-cropperjs";
+import "cropperjs/dist/cropper.css";
+
 export default {
   name: "home",
   components: {
     SavedPost,
     UserCreated,
-    Setting
+    Setting,
+    VueCropper
   },
   data() {
     return {
-      items: []
+      showModal: false,
+      items: [],
+      cropperImg: "/assets/images/default.jpg",
+      croperType: "avatar",
+      aspectRatio: 1 / 1
     };
   },
   computed: {
@@ -163,7 +214,44 @@ export default {
   },
   methods: {
     ...mapMutations(["SET_PAGE"]),
-    ...mapActions(["LoadUserInfo", "ChangeAvatar", "ChangeCover"])
+    ...mapActions(["LoadUserInfo", "ChangeAvatar", "ChangeCover"]),
+    selectImage(type) {
+      if (type === "avatar") {
+        this.croperType = "avatar";
+        this.aspectRatio = 1 / 1;
+      }
+      if (type === "cover") {
+        this.croperType = "cover";
+        this.aspectRatio = 16 / 9;
+      }
+      this.$refs.cropper.setAspectRatio(this.aspectRatio);
+      this.$refs.imageInput.click();
+    },
+    onImageSelect(e) {
+      const file = e.target.files[0];
+      if (file.type.indexOf("image/") === -1) {
+        alert("Please select an image file");
+        return;
+      }
+      if (typeof FileReader === "function") {
+        const reader = new FileReader();
+        let self = this;
+        reader.onload = event => {
+          self.cropperImg = event.target.result;
+          // rebuild cropperjs with the updated source
+          self.$refs.cropper.replace(event.target.result);
+          self.showModal = true;
+        };
+        reader.readAsDataURL(file);
+      } else {
+        alert("Sorry, FileReader API not supported");
+      }
+    },
+
+    cropImage() {
+      // get image data for post processing, e.g. upload or setting image src
+      console.log(this.$refs.cropper.getCroppedCanvas().toDataURL());
+    }
   }
 };
 </script>
