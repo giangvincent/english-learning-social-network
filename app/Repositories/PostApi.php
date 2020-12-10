cd234eb6d54e5475b29bd7c1e06ba0000bb54dba
mfvodka107 <giang.dlinh92@gmail.com>
2020-12-10 16:41:47 +0700

    ADD repositories api

---
+++ app/Repositories/PostApi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Http\Controllers\Controller;
+
+class PostApi extends Controller
+{
+
+}
---
+++ app/Repositories/UserApi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Http\Controllers\Controller;
+
+class UserApi extends Controller
+{
+
+}
