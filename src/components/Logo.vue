6783041f3acb5e8e58e794a368ffc639e7297881
mfvodka107 <giang.dlinh92@gmail.com>
2020-12-23 11:45:22 +0700

    change icons and logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,5 +2,6 @@
   <router-link
     to="/"
     class="items-center hover:no-underline font-bold text-gray-800 text-xl uppercase"
-  >ohitsgood</router-link>
+    ><img class="h-8" src="@/assets/images/logo.jpg" alt="THATSGOOD" />
+  </router-link>
 </template>
