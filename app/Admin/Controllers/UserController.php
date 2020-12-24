cbe979d6e46e4e93c45cb743166a2fd2a3ec9243
mfvodka107 <giang.dlinh92@gmail.com>
2020-12-24 14:46:43 +0700

    user management

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
+use Encore\Admin\Widgets\Table;
 
 class UserController extends AdminController
 {
@@ -27,16 +28,35 @@ class UserController extends AdminController
         $grid = new Grid(new User());
 
         $grid->column('id', __('Id'));
-        $grid->column('full_name', __('Full name'));
+        $grid->column('full_name', __('Full name'))->modal('Details', function ($detail) {
+            $userInfo = $detail->info()->select('bio', 'socials_conn', 'notification_conn', 'others', 'cover_image')->first()->toArray();
+            $userInfo['bagged'] = $detail->interacts()->where('interact', 'bagged')->count();
+            $userInfo['vote_good'] = $detail->interacts()->where('interact', 'good')->count();
+            $userInfo['vote_bad'] = $detail->interacts()->where('interact', 'bad')->count();
+            $userInfo['progress'] = $detail->progress()->count();
+            return new Table(['Detail', 'content'], $userInfo);
+        });
         $grid->column('nick_name', __('Nick name'));
-        $grid->column('avatar', __('Avatar'));
+        $grid->column('avatar', __('Avatar'))->image('/../', 100, 100);
+        $grid->column('phone', __('Phone'));
+        $grid->column('birthday', __('Birth day'));
         $grid->column('email', __('Email'));
-        $grid->column('email_verified_at', __('Email verified at'));
-        $grid->column('password', __('Password'));
-        $grid->column('remember_token', __('Remember token'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+        $grid->column('created_at', __('Created at'))->display(function ($created_at) {
+            return date("Y-m-d H:i:s", strtotime($created_at));
+        });
+        $grid->column('updated_at', __('Updated at'))->display(function ($created_at) {
+            return date("Y-m-d H:i:s", strtotime($created_at));
+        });
 
+        $grid->quickSearch('full_name', 'email');
+        $grid->filter(function ($filter) {
+            $filter->like('full_name');
+            $filter->like('nick_name');
+            $filter->like('email');
+
+            $filter->between('created_at', 'Lọc theo ngày đăng ký')->datetime();
+
+        });
         return $grid;
     }
 
