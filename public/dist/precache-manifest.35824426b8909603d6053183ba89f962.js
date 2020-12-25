a9ad80b994b205c8bff56f0c2e246efd04feb3c7
mfvodka107 <giang.dlinh92@gmail.com>
2020-12-25 15:51:32 +0700

    synced + change api to api. domain

---
+++ public/dist/precache-manifest.35824426b8909603d6053183ba89f962.js
@@ -0,0 +1,406 @@
+self.__precacheManifest = [
+  {
+    "revision": "509a160f4240c0fe518c",
+    "url": "/css/app.195386d8.css"
+  },
+  {
+    "revision": "509a160f4240c0fe518c",
+    "url": "/js/app.fab020b3.js"
+  },
+  {
+    "revision": "26bbb5b5268b0f5e9ef8",
+    "url": "/css/chunk-02603fca.594f2fe4.css"
+  },
+  {
+    "revision": "26bbb5b5268b0f5e9ef8",
+    "url": "/js/chunk-02603fca.3cb080fd.js"
+  },
+  {
+    "revision": "a6aaefea606807b6e1fc",
+    "url": "/js/chunk-04e99fe7.0a86a817.js"
+  },
+  {
+    "revision": "11fc46072368ae58063f",
+    "url": "/js/chunk-24c79eb1.25cb880b.js"
+  },
+  {
+    "revision": "2acfa8f945c8f03afdf1",
+    "url": "/js/chunk-2d0b6003.af0022cd.js"
+  },
+  {
+    "revision": "cc4c1ae2f1f432160bba",
+    "url": "/js/chunk-2d0c1d78.358e0c7b.js"
+  },
+  {
+    "revision": "8a0853c7a0aee4d71a0c",
+    "url": "/js/chunk-2d0d3ae0.6d97f5ae.js"
+  },
+  {
+    "revision": "0f050a8b9ead0932e191",
+    "url": "/js/chunk-2d0dd3b6.d3bf8401.js"
+  },
+  {
+    "revision": "c76af902aaea0f1420dd",
+    "url": "/js/chunk-2d0e9cd9.057d005a.js"
+  },
+  {
+    "revision": "1728914c2a820356c800",
+    "url": "/js/chunk-2d21e7f1.4258b37a.js"
+  },
+  {
+    "revision": "499179978fa3d784e0e5",
+    "url": "/css/chunk-4128aaa3.f10d6c7d.css"
+  },
+  {
+    "revision": "499179978fa3d784e0e5",
+    "url": "/js/chunk-4128aaa3.c201a903.js"
+  },
+  {
+    "revision": "774640930fdf15826893",
+    "url": "/css/chunk-5ac3e769.fe81da84.css"
+  },
+  {
+    "revision": "774640930fdf15826893",
+    "url": "/js/chunk-5ac3e769.646c5535.js"
+  },
+  {
+    "revision": "5fb60969797a130497a6",
+    "url": "/js/chunk-5bbea854.e95a5c53.js"
+  },
+  {
+    "revision": "db3b1197e27718f14545",
+    "url": "/js/chunk-6061996e.12d83314.js"
+  },
+  {
+    "revision": "7fd4137bb481e5290756",
+    "url": "/js/chunk-84ca1938.6f35f759.js"
+  },
+  {
+    "revision": "22cd93db9e014cef8f01",
+    "url": "/js/chunk-87f10f72.d5addf3e.js"
+  },
+  {
+    "revision": "255ecebcfb2cc54ac3df",
+    "url": "/js/chunk-a9a56bd6.dfdec900.js"
+  },
+  {
+    "revision": "fea2811404a59ae8469c",
+    "url": "/css/chunk-vendors.2619ac45.css"
+  },
+  {
+    "revision": "fea2811404a59ae8469c",
+    "url": "/js/chunk-vendors.f37b11cd.js"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/fonts/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/fonts/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/fonts/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/fonts/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "0d6e6ed9f75739cc2a0666e0ab48dac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58580961b3bc41c32b760010b9c2199c",
+    "url": "/assets/icons/add_col_after.svg"
+  },
+  {
+    "revision": "85fa4a994966a0919b08079241110867",
+    "url": "/assets/icons/add_col_before.svg"
+  },
+  {
+    "revision": "125aac7e267cc8dbdd4738aebea1224e",
+    "url": "/assets/icons/add_row_before.svg"
+  },
+  {
+    "revision": "ad34da96097c43c0ea9dbbcd0c548460",
+    "url": "/assets/fabric.backup.min.js"
+  },
+  {
+    "revision": "62525792dd1aa2c49a71877d598c96e5",
+    "url": "/assets/icons/add_row_after.svg"
+  },
+  {
+    "revision": "f6060025a07a6fcdd45cb5b416619a18",
+    "url": "/assets/fabric.min.js"
+  },
+  {
+    "revision": "17e112ecb9e86536a4f42817819058db",
+    "url": "/assets/icons/checklist.svg"
+  },
+  {
+    "revision": "3bcc3ecffca85f1a7f387941b98d6fbb",
+    "url": "/assets/icons/bold.svg"
+  },
+  {
+    "revision": "cd105f12c1409d0c564651b2f1e05521",
+    "url": "/assets/icons/combine_cells.svg"
+  },
+  {
+    "revision": "cf9c576d7892454d9f842b3dd5f48e12",
+    "url": "/assets/icons/code.svg"
+  },
+  {
+    "revision": "7d1a8c441cf332c8b0a1de551dbc0353",
+    "url": "/assets/icons/delete_col.svg"
+  },
+  {
+    "revision": "fe3045db3af2fcd0012b9295e3703b72",
+    "url": "/assets/icons/delete_row.svg"
+  },
+  {
+    "revision": "157c70b30e4cd32dd2a7d9d018f2258a",
+    "url": "/assets/icons/delete_table.svg"
+  },
+  {
+    "revision": "9b540ce4d8809e2a744b350ca5f975d0",
+    "url": "/assets/icons/fb_share.png"
+  },
+  {
+    "revision": "d198915f9e6bb6f50b0510c871827642",
+    "url": "/assets/icons/github.svg"
+  },
+  {
+    "revision": "cc944d351c93ff3069066b852485e311",
+    "url": "/assets/icons/hr.svg"
+  },
+  {
+    "revision": "cee3473229c5f145de6998c492199c8d",
+    "url": "/assets/icons/image.svg"
+  },
+  {
+    "revision": "8752a6f27750e23ef6f0bb0ae39d8ef9",
+    "url": "/assets/icons/link.svg"
+  },
+  {
+    "revision": "880f7a1d072bbbb7c56753aa0ef54a95",
+    "url": "/assets/icons/italic.svg"
+  },
+  {
+    "revision": "60379eca28576faf75de97be9e493369",
+    "url": "/assets/icons/mention.svg"
+  },
+  {
+    "revision": "fe1a5f990ee6cb688b7fa1d9a07568e2",
+    "url": "/assets/icons/ol.svg"
+  },
+  {
+    "revision": "5d0178c09d24e915a79f10e5a4cf3cd7",
+    "url": "/assets/icons/paragraph.svg"
+  },
+  {
+    "revision": "025207207a2343344b341243daeb52d9",
+    "url": "/assets/icons/quote.svg"
+  },
+  {
+    "revision": "caa8775391daa070b6748a8d3cd81c42",
+    "url": "/assets/icons/redo.svg"
+  },
+  {
+    "revision": "b5ab31f3b3a7b8e973d63710d2182416",
+    "url": "/assets/icons/remove.svg"
+  },
+  {
+    "revision": "bd4b7d9fdf72e2ca35059c617e589cf8",
+    "url": "/assets/icons/strike.svg"
+  },
+  {
+    "revision": "3a51a5740aff2b61c96f051aa5365a4b",
+    "url": "/assets/icons/table.svg"
+  },
+  {
+    "revision": "7f4ae06eee4b0db2235cfbed82b8d38a",
+    "url": "/assets/icons/twittershare.png"
+  },
+  {
+    "revision": "19102aeebe7f20d3cebe92b2b6507d9c",
+    "url": "/assets/icons/ul.svg"
+  },
+  {
+    "revision": "d1faa2d7ae133bdbe20782d56c2f5d92",
+    "url": "/assets/icons/underline.svg"
+  },
+  {
+    "revision": "bcfc20289d6f50ce631ad483e211f03c",
+    "url": "/assets/icons/undo.svg"
+  },
+  {
+    "revision": "cb8218a02e6280befb13d57b7b51e579",
+    "url": "/assets/icons/zalo_share.png"
+  },
+  {
+    "revision": "6c383b8af56e5cd0b9381a68bf420884",
+    "url": "/assets/images/default_avatar.jpg"
+  },
+  {
+    "revision": "b0653b28038b5421f9218e58dab6f0d6",
+    "url": "/assets/images/default-vertical.jpg"
+  },
+  {
+    "revision": "e9d90ae4e0efcf595b3e8a14a04d90a9",
+    "url": "/assets/images/logo.jpg"
+  },
+  {
+    "revision": "b81129963e1108f7c5c9fb46be93ef9d",
+    "url": "/content/about.json"
+  },
+  {
+    "revision": "be808588ebf6ba7270e1441077b3481d",
+    "url": "/content/categories.json"
+  },
+  {
+    "revision": "b307ea1ba075e3d5988595f6694148f3",
+    "url": "/assets/images/default.jpg"
+  },
+  {
+    "revision": "e4b729e5df4196e9685e29ba777f98fa",
+    "url": "/content/categories/nghe.json"
+  },
+  {
+    "revision": "1abdc0b6ad1e1f6eb653556548990f91",
+    "url": "/content/categories/ngu-phap.json"
+  },
+  {
+    "revision": "72469d88016c173663dfcf0a8845a32e",
+    "url": "/content/faqs.json"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/content/feed/home.json"
+  },
+  {
+    "revision": "8efa935a4a5219691cdd91ccd302341c",
+    "url": "/content/notification/1.json"
+  },
+  {
+    "revision": "d751713988987e9331980363e24189ce",
+    "url": "/content/notification/4.json"
+  },
+  {
+    "revision": "1057b794eb3de80374ba4ab7ac9000e7",
+    "url": "/content/categories/tu-vung.json"
+  },
+  {
+    "revision": "89d9667b5a431347659164932332f3f0",
+    "url": "/content/policy.json"
+  },
+  {
+    "revision": "6a50aaa843824eb960788162b2287d7d",
+    "url": "/content/posts/1c1b521a-647a-4f4b-8865-db25a777f853.json"
+  },
+  {
+    "revision": "5307d95e79ab8e782c5688ee95b7c3bd",
+    "url": "/content/posts/1613c278-394b-4f56-814b-27ed4a1b7f13.json"
+  },
+  {
+    "revision": "b1f9b4dcbe398ab182b57b23f8147cb4",
+    "url": "/content/categories/bai-viet.json"
+  },
+  {
+    "revision": "bdc209c1a7d8bb8d01acce8b44d6a628",
+    "url": "/content/posts/295f369d-3c98-4f41-b8e3-c1998a94677f.json"
+  },
+  {
+    "revision": "2e068b3d3e97dbd3d40331d37061a169",
+    "url": "/content/posts/2f2b95f5-2623-4d33-9b58-f489b95150c0.json"
+  },
+  {
+    "revision": "ed396134c566aed2c09dcabe85b74530",
+    "url": "/content/posts/30476805-aeda-4fb2-8f72-a2164601ddff.json"
+  },
+  {
+    "revision": "027bb23774c71dfae672855fc8162a09",
+    "url": "/content/posts/7ce9569f-860e-40ba-80d0-8790dca633ee.json"
+  },
+  {
+    "revision": "7b887ccbfa9edb1020207b0097f0da27",
+    "url": "/content/posts/8a95794b-e6eb-4f88-9ef7-f0755398ce5b.json"
+  },
+  {
+    "revision": "34484a1de5845171a3f628e4f9ed38ac",
+    "url": "/content/posts/b1668730-4c17-41c2-bbc8-333c1f77bcc2.json"
+  },
+  {
+    "revision": "973e1c24881e78ab3dfee9377c4bffa9",
+    "url": "/content/posts/71f3be46-8e65-444d-bfbc-9dd988889d9f.json"
+  },
+  {
+    "revision": "1e67e1d355300ea3948374852750227f",
+    "url": "/content/posts/b569c2d0-347b-4e4d-973d-4672d14826b3.json"
+  },
+  {
+    "revision": "a273ab2c7ee95106e420e6b190bf6ac4",
+    "url": "/content/posts/bb1eae29-a8b0-4805-9063-98925ada76de.json"
+  },
+  {
+    "revision": "60a7e763159a24f0d31c31166139513a",
+    "url": "/content/posts/fb800285-eecf-4fab-a623-4a37b0637be3.json"
+  },
+  {
+    "revision": "cdd5197d208e8feeeeaa221acda25a79",
+    "url": "/content/progress/1_295f369d-3c98-4f41-b8e3-c1998a94677f.json"
+  },
+  {
+    "revision": "19414e9a064de1df140aba5444e958fe",
+    "url": "/content/tags.json"
+  },
+  {
+    "revision": "ec3abb4696c054641c126a9934b5e959",
+    "url": "/content/posts/786ad381-9498-4d29-8bcf-70013e8c9561.json"
+  },
+  {
+    "revision": "c404fd22f3633c2f4676c0509c437b2d",
+    "url": "/content/tags/activity.json"
+  },
+  {
+    "revision": "87cfd2215b56dea7923b61449304288f",
+    "url": "/content/posts/ba906f04-e813-4f2b-951a-9cf300a571b7.json"
+  },
+  {
+    "revision": "36d4007298bf311605d057ea791fcc93",
+    "url": "/content/tags/ada.json"
+  },
+  {
+    "revision": "366d32fd6cd6084082efcce99d9c9656",
+    "url": "/content/tags/animal.json"
+  },
+  {
+    "revision": "0aa96c5690500e0d354353e8db57193c",
+    "url": "/content/tags/basic-greeting.json"
+  },
+  {
+    "revision": "91c03606672f548aa9248c6358be4500",
+    "url": "/content/tags/dong-vat.json"
+  },
+  {
+    "revision": "facf6c2d076b2d2528660e7b17cce104",
+    "url": "/content/tags/food.json"
+  },
+  {
+    "revision": "9681ab3d0086f46b9f74f81b208a2438",
+    "url": "/content/tags/video.json"
+  },
+  {
+    "revision": "b42906f56b3b00fe981e0472107f2f4c",
+    "url": "/icon.ico"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e08cc1f4523064d58d5144057d5df130",
+    "url": "/thatsgood_logo_icon.ico"
+  }
+];
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JsonController;
 
 /*
 |--------------------------------------------------------------------------
@@ -51,3 +51,29 @@ Route::group(['middleware' => 'auth:api'], function () {
 Route::get('feed-home', 'FeedController@home');
 Route::get('feed-category/{slug}', 'FeedController@feedCategory');
 Route::get('feed-tag/{slug}', 'FeedController@feedTag');
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/share/{postId}', function ($postId) {
+    $postJson = new JsonController();
+    $data = $postJson->getPost($postId);
+
+    if (!is_array($data)) {
+        $data = json_decode($data, true)[0];
+    }
+    return view('share', ['data' => $data]);
+});
+
+Route::group([
+    'prefix' => 'get-json',
+], function () {
+    Route::get('post/{id}', [JsonController::class, 'getPost']);
+    Route::get('category/{slug}', [JsonController::class, 'getCategory']);
+    Route::get('categories', [JsonController::class, 'getCategories']);
+    Route::get('tag/{slug}', [JsonController::class, 'getTag']);
+    Route::get('tags', [JsonController::class, 'getTags']);
+
+    Route::get('progress/{link}', [JsonController::class, 'getUserProgress']);
+
+    Route::get('notification/{link}', [JsonController::class, 'getUserNotification']);
+});
