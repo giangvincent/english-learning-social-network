8b28f936ed0ce9b2585b0ea2e4a11fb4b788258f
mfvodka107 <giang.dlinh92@gmail.com>
2021-01-06 12:26:34 +0700

    fix play uadio in flashcard + quiz

--- src/components/Feed/FlashCard.vue
+++ src/components/Feed/FlashCard.vue
@@ -35,12 +35,13 @@
         </div>
         <button
           v-if="
-            typeof postData.audios !== 'undefined' &&
-              typeof postData.audios[0] !== 'undefined' &&
-              postData.audios[0] !== ''
+            typeof postData.content[currentCardIndex].audios !== 'undefined' &&
+              typeof postData.content[currentCardIndex].audios[0] !==
+                'undefined' &&
+              postData.content[currentCardIndex].audios[0] !== ''
           "
           class="p-1 ml-3 mb-2 flex items-center rounded-lg border-2 hover:border-gray-600 py-2 border-gray-300 bg-white"
-          @click="playAudio(postData.audios[0])"
+          @click="playAudio(postData.content[currentCardIndex].audios[0])"
         >
           <svg
             class="w-8"
@@ -106,12 +107,14 @@
         </div>
         <button
           v-if="
-            typeof postData.flipAudios !== 'undefined' &&
-              typeof postData.flipAudios[0] !== 'undefined' &&
-              postData.flipAudios[0] !== ''
+            typeof postData.content[currentCardIndex].flipAudios !==
+              'undefined' &&
+              typeof postData.content[currentCardIndex].flipAudios[0] !==
+                'undefined' &&
+              postData.content[currentCardIndex].flipAudios[0] !== ''
           "
           class="p-1 ml-3 mb-2 flex items-center rounded-lg border-2 hover:border-gray-600 py-2 border-gray-300 bg-white"
-          @click="playAudio(postData.flipAudios[0])"
+          @click="playAudio(postData.content[currentCardIndex].flipAudios[0])"
         >
           <svg
             class="w-8"
--- src/components/Feed/Quiz.vue
+++ src/components/Feed/Quiz.vue
@@ -22,12 +22,13 @@
         </div>
         <button
           v-if="
-            typeof postData.audios !== 'undefined' &&
-              typeof postData.audios[0] !== 'undefined' &&
-              postData.audios[0] !== ''
+            typeof postData.content[currentQuizIndex].audios !== 'undefined' &&
+              typeof postData.content[currentQuizIndex].audios[0] !==
+                'undefined' &&
+              postData.content[currentQuizIndex].audios[0] !== ''
           "
           class="p-1 ml-3 mb-2 flex items-center rounded-lg border-2 hover:border-gray-600 py-2 border-gray-300 bg-white"
-          @click="playAudio(postData.audios[0])"
+          @click="playAudio(postData.content[currentQuizIndex].audios[0])"
         >
           <svg
             class="w-8"
