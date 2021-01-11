f494b83609177b1136a2fbf05b766f54ebe482b2
mfvodka107 <giang.dlinh92@gmail.com>
2021-01-11 14:30:06 +0700

    fix botController

--- app/Http/Controllers/API/BotController.php
+++ app/Http/Controllers/API/BotController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
 
 class BotController extends Controller
 {
@@ -16,7 +18,7 @@ class BotController extends Controller
 
     public function __construct()
     {
-        $this->token = 'SOMETHING_RANDOM';
+        $this->token = 'thatsgood_token';
     }
 
     /**
@@ -47,10 +49,14 @@ class BotController extends Controller
     {
         $entry = $request->get('entry');
 
-        $sender = array_get($entry, '0.messaging.0.sender.id');
-        // $message = array_get($entry, '0.messaging.0.message.text');
-
-        $this->dispatchResponse($sender, 'Hello world. You can customise my response.');
+        $sender = Arr::get($entry, '0.messaging.0.sender.id');
+        Log::info('start handle entry : ' . json_encode($entry));
+        $message = mb_strtolower(Arr::get($entry, '0.messaging.0.message.text'), 'UTF-8');
+        if ($message === 'register' || $message === 'đăng ký') {
+            $this->dispatchResponse($sender, 'Cảm ơn bạn đã đăng ký. Tin nhắn để nhắc nhở bạn học hàng ngày sẽ được gửi đi đều đặn. Nhớ truy cập <a href="https://thatsgood.info">https://thatsgood.info</a> nhé!');
+        } else {
+            $this->dispatchResponse($sender, 'Sai cú pháp! Xin vui lòng gõ "đăng ký" hoặc "register". Xin cảm ơn.');
+        }
 
         return response('', 200);
     }
@@ -64,10 +70,11 @@ class BotController extends Controller
      */
     protected function dispatchResponse($id, $response)
     {
-        $access_token = env('BOT_PAGE_ACCESS_TOKEN');
+        $access_token = 'EAAL60j78yO8BACTyjvccWmsLsrpLbkfbQUyp1BknwohzYhZAywQZAQtZBLGBZAgZCSSer5hvAXgG4IrcCTvaN3DBi7zN0yGJjmGRqkFJTcFmxLhkmYpR4mDn96jzoNDu8EsAonTe0c3Doj64wQWVZCg4Q3KeZBDGsyNjh0Wsbgxwir7bi0DKHAwXWdDR7MxOLAZD';
         $url = "https://graph.facebook.com/v9.0/me/messages?access_token={$access_token}";
 
         $data = json_encode([
+            "messaging_type" => 'UPDATE',
             'recipient' => ['id' => $id],
             'message' => ['text' => $response],
         ]);
