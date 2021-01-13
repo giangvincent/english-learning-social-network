623d28fc829c7df8d045f90b029cafbae4e7536e
mfvodka107 <giang.dlinh92@gmail.com>
2021-01-13 12:50:15 +0700

    fix copy link share

--- src/components/Feed/BaseParts/InteractionPack.vue
+++ src/components/Feed/BaseParts/InteractionPack.vue
@@ -67,14 +67,14 @@
               />
             </svg>
           </li>
-          <!-- <li class="m-1">
+          <li class="m-1 fixed z-0" style="left: -99999px">
             <input
               class="h-10"
               ref="linkShare"
               type="text"
               :value="linkShare"
             />
-          </li> -->
+          </li>
         </ul>
       </div>
     </div>
