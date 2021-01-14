151bb2bd62cf2c016f4e7588268b07069ed72020
mfvodka107 <giang.dlinh92@gmail.com>
2021-01-14 13:59:52 +0700

    fix logo size for mobile

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,6 +2,10 @@
   <router-link
     to="/"
     class="items-center hover:no-underline font-bold text-gray-800 text-xl uppercase"
-    ><img class="h-6 my-2" src="@/assets/images/logo.png" alt="THATSGOOD" />
+    ><img
+      class="h-4 sm:h-6 my-2"
+      src="@/assets/images/logo.png"
+      alt="THATSGOOD"
+    />
   </router-link>
 </template>
